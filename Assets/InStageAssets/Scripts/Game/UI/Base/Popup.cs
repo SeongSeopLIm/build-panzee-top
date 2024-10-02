23f843d00ecec0e0e377b159d3dedfbe58d719dd
Leaf <tjdtjq96@gmail.com>
2024-10-02 14:57:45 +0900

    오디오 매니저 추가, UI 인터렉트 이펙트 추가

---
+++ Assets/InStageAssets/Scripts/Game/UI/Base/Popup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAK.UI
+{
+
+    public class PopupData : WindowData
+    {
+
+    }
+
+    [UIView(id: nameof(Popup), path: "Prefabs/UI/Popup", dataType: typeof(PopupData), isSingleInstance: true)]
+    public class Popup : View
+    { 
+        protected override void OnSetData(ViewData viewData)
+        {
+            base.OnSetData(viewData);
+        } 
+    }
+}
---
+++ Assets/InStageAssets/Scripts/Game/UI/SettingPopup.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using WAK.Managers;
+
+namespace WAK.UI
+{
+    public class SettingPopupData : PopupData
+    {
+
+    }
+
+    [UIView(id: nameof(SettingPopup), path: "Prefabs/UI/SettingPopup", dataType: typeof(SettingPopupData))]
+    public class SettingPopup : Window
+    {
+        [SerializeField] private Slider BGMSlider;
+        [SerializeField] private Slider SFXSlider;
+
+        protected override void AddListeners()
+        {
+            base.AddListeners();
+        }
+
+
+        protected override void OnSetData(ViewData viewData)
+        {
+            base.OnSetData(viewData);
+            
+            BGMSlider
+                .OnValueChangedAsObservable()
+                .Subscribe()
+                .AddTo(disposable);
+            SFXSlider
+                .OnValueChangedAsObservable()
+                .Subscribe()
+                .AddTo(disposable);
+        }
+
+        protected override void OnClear()
+        {
+            base.OnClear();
+        }
+    }
+}
